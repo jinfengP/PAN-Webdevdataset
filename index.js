2fc14558fa9266d3624c21306f998876db81ee8b
PanMan <146111571+jinfengP@users.noreply.github.com>
2024-12-20 15:04:06 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+yearsList = []
+tempList = []
+
 async function getData() {
     const response = await fetch("jinfeng.csv");
     const data = await response.text();
@@ -6,9 +9,39 @@ async function getData() {
               const row = elem.split(",");
               const year = row[0];
               const temp = row[1];
-              console.log(year, temp);
+              console.log(year + " " + temp)
+              yearsList.push(year)
+              tempList.push(temp)
             });
           }
     //end of functions
     //call the function to test if you see the image on the left in the console
-getData()
+
+async function makeChart() {
+getData();
+const ctx = document.getElementById('myChart');
+console.log(yearsList)
+console.log(tempList)
+//codeblock for the chart
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: yearsList,
+    datasets: [{
+      label: 'Temperature Change',
+      data: tempList,
+      borderWidth: 1,
+      backgroundColor: "#00D816",
+      borderColor: "#F700C6"
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+})}
+
+makeChart();
